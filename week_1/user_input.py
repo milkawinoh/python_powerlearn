83c054718bd9fefa3a5d05a8c551ccdbd3447e22
milkawinoh <milkawinoh@gmail.com>
2024-02-26 13:09:38 +0300

    initial commit

---
+++ week_1/user_input.py
@@ -0,0 +1,4 @@
+name = input("what is your name? ")
+age = input("what is your age? ")
+location = input("what is your location? ")
+print("Hello {} you are {} years old and live in {}".format(name,age,location))
